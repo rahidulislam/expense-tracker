ce5a6453a2f7c89934ec2719a6e45b681f1939a1
Rahidul Islam <rahiseli@outlook.com>
2023-03-13 09:42:13 +0600

    create async thunks

---
+++ src/features/transaction/TransactionSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+    getTransactions,
+    addTransaction,
+    editTransaction,
+    deleteTransaction,
+} from "./TransactionAPI";
+
+// initialize
+initialState = {
+    transactions: [],
+    isLoading: false,
+    isError: false,
+    errorMessage: "",
+};
+
+// create thunks
+export const fetchTransactions = createAsyncThunk(
+    "transaction/fetchTransactions",
+    async () => {
+        const transactions = await getTransactions();
+        return transactions;
+    }
+);
+
+export const createTransaction = createAsyncThunk(
+    "transaction/createTransaction",
+    async (data) => {
+        const transaction = await addTransaction(data);
+        return transaction;
+    }
+);
+
+export const changeTransaction = createAsyncThunk(
+    "transaction/changeTransaction",
+    async ({ id, data }) => {
+        const transaction = await editTransaction(id, data);
+        return transaction;
+    }
+);
+
+export const removeTransaction = createAsyncThunk(
+    "transaction/removeTransaction",
+    async (id) => {
+        const transaction = await deleteTransaction(id);
+        return transaction;
+    }
+);
+//
