f3ad6798887999c488768059132dace25e6235fc
Rahidul Islam <rahiseli@outlook.com>
2023-03-15 08:17:36 +0600

    add transaction is completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import {createTransaction} from "../features/transaction/transactionSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { createTransaction } from "../features/transaction/transactionSlice";
 export default function Form() {
     const [name, setName] = useState("");
     const [type, setType] = useState("");
     const [amount, setAmount] = useState("");
     const dispatch = useDispatch();
+    const {isLoading, isError, errorMessage} = useSelector(state=>state.transaction)
     const handleCreate = (e) => {
         e.preventDefault();
         dispatch(
             createTransaction({
                 name,
                 type,
-                amount,
+                amount:Number(amount),
             })
         );
     };
     return (
         <div className="form">
-            <Form onSubmit={handleCreate}>
-                <h3>Add new transaction</h3>
-
+            <h3>Add new transaction</h3>
+            <form onSubmit={handleCreate}>
                 <div className="form-group">
                     <label for="transaction_name">Name</label>
                     <input
                         type="text"
                         name="transaction_name"
+                        required
                         placeholder="My Salary"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
@@ -40,7 +41,7 @@ export default function Form() {
                             value="income"
                             name="transaction_type"
                             checked={type === "income"}
-                            onChange={setType("income")}
+                            onChange={(e) => setType("income")}
                         />
                         <label for="transaction_type">Income</label>
                     </div>
@@ -51,7 +52,7 @@ export default function Form() {
                             name="transaction_type"
                             placeholder="Expense"
                             checked={type === "expense"}
-                            onChange={setType("expense")}
+                            onChange={(e) => setType("expense")}
                         />
                         <label for="transaction_type">Expense</label>
                     </div>
@@ -63,13 +64,15 @@ export default function Form() {
                         type="number"
                         placeholder="300"
                         name="transaction_amount"
+                        required
                         value={amount}
                         onChange={(e) => setAmount(e.target.value)}
                     />
                 </div>
 
-                <button className="btn">Add Transaction</button>
-            </Form>
+                <button className="btn" type="submit" disabled={isLoading}>Add Transaction</button>
+                {!isLoading && isError && <p className="error">{errorMessage}</p>}
+            </form>
             <button className="btn cancel_edit">Cancel Edit</button>
         </div>
     );
